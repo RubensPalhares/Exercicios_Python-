ea376fe2e0eb1d5a3c2cc2949369e78527aef77a
Rubens Palhares <80899757+RubensPalhares@users.noreply.github.com>
2022-09-15 14:37:38 -0300

    Add files via upload

---
+++ ex02.py
@@ -0,0 +1,12 @@
+
+# 2) Escreva uma função que recebe um número n como parâmetro e
+#  imprime se n é positivo ou negativo.
+
+def imprime_sinal(n):
+    if n < 0:
+      print("O número é Negativo")
+    else:
+      print("O númeto é Positivo")
+
+imprime_sinal(-1)
+imprime_sinal(5)
