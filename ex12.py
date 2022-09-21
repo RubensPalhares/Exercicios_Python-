8bcf910f70f0ec3288b7de42a6693efd36fa2688
Rubens Palhares <80899757+RubensPalhares@users.noreply.github.com>
2022-09-21 17:51:03 -0300

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,8 @@
+#12) Laços while
+#Além dos laços for, Python conta também com os laços while,
+# que executam enquanto uma dada condição for verdadeira:
+
+i = 1
+while i < 5:
+    print(i)
+    i = i + 1
